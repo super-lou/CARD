fde4afcbb541e22d2d865b5bd0ac79df78ecdf5c
louis.heraut <louis.heraut@inrae.fr>
2023-03-24 00:09:02 +0100

    minor modif

--- __all__/Q/QA_MON.R
+++ __all__/Q/QA_MON.R
@@ -6,6 +6,5 @@ CARD$P.topic = "Moyennes Eaux"
 CARD$P1.funct = list(QA_MON=mean)
 CARD$P1.funct_args = list("Q", na.rm=TRUE)
 CARD$P1.timeStep = "year-month"
-CARD$P1.compress = TRUE
 CARD$P1.NApct_lim = 3
 CARD$P1.NAyear_lim = 10
--- __all__/Q/QA_SEA.R
+++ __all__/Q/QA_SEA.R
@@ -5,8 +5,7 @@ CARD$P.topic = c("Moyennes Eaux", "Liés à une statistique")
 
 CARD$P1.funct = list(QA_SEA=mean)
 CARD$P1.funct_args = list("Q", na.rm=TRUE)
-CARD$P1.timeStep = "season"
+CARD$P1.timeStep = "year-season"
 CARD$P1.Seasons = c("DJF", "MAM", "JJA", "SON")
-CARD$P1.compress = TRUE
 CARD$P1.NApct_lim = 3
 CARD$P1.NAyear_lim = 10
