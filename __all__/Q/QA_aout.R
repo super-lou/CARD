06934a961058b0ce4498e49a023cff8a0126332d
louis.heraut <louis.heraut@inrae.fr>
2023-04-24 18:24:31 +0200

    remove accent

--- __all__/Q/QA_aout.R
+++ __all__/Q/QA_aout.R
@@ -1,10 +1,10 @@
-CARD$P.var = "QA_août"
+CARD$P.var = "QA_aout"
 CARD$P.unit = "m^{3}.s^{-1}"
 CARD$P.glose = "Moyenne des débits journaliers de chaque mois d'août"
 CARD$P.topic = "Moyennes Eaux"
 CARD$P.samplePeriod = c("08-01", "08-31")
     
-CARD$P1.funct = list("QA_août"=mean)
+CARD$P1.funct = list("QA_aout"=mean)
 CARD$P1.funct_args = list("Q", na.rm=TRUE)
 CARD$P1.timeStep = "year"
 CARD$P1.samplePeriod = c("08-01", "08-31")
