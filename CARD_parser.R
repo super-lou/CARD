485eeb0b4aea45bddf2a5b568a4de04c3a084e73
louis.heraut <louis.heraut@inrae.fr>
2023-05-12 08:32:23 +0200

    change card parser

---
+++ CARD_parser.R
@@ -0,0 +1,54 @@
+#!/usr/bin/env Rscript   
+# use chmod ug+x to make this file executable
+
+
+# Copyright 2022-2023 Louis Héraut (louis.heraut@inrae.fr)*1
+#
+# *1   INRAE, France
+#
+# This file is part of CARD R library.
+#
+# CARD R library is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# CARD R library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CARD R library.
+# If not, see <https://www.gnu.org/licenses/>.
+
+
+suppressPackageStartupMessages(library(argparse))
+
+parser = ArgumentParser()
+
+parser$add_argument('-C', '--CARD', type="character",
+                    default=".",
+                    help="CARD directory.")
+parser$add_argument('-t', '--type', type="integer",
+                    default=1,
+                    help="1 for analyse of one variable with CARD contained in the __all__/1/ directory or 2 for analyse of two variables with CARD contained in the __all__/2/ directory.")
+parser$add_argument('-l', '--layout', nargs='+', type="character",
+                    default="EX [ QA ]",
+                    help="Layout of the chosen variables.")
+parser$add_argument("-w", "--white", action="store_true", default=TRUE,
+                    help="Underscores in directory names are replace by white spaces")
+parser$add_argument("-b", "--blank", action="store_true", default=FALSE,
+                    help="Remove id before variable names")
+parser$add_argument("-v", "--verbose", action="store_true", default=FALSE,
+                    help="Print information")
+
+args = parser$parse_args()
+
+# print(args$l)
+# args$l = c("A", "[", "a", "[", "aa", "[", "aaa", "bbb", "]", "bb", "[", "ccc", "ddd", "]", "]", "b", "[", "aa", "[", "aaa", "]", "]", "]")
+
+
+source("/home/louis/Documents/bouleau/INRAE/project/EXstat_project/EXstat/R/CARD_management.R")
+CARD_management(args=args)
+# warnings()
