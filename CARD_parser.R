a0a80dd245c9094d637798648628d3343e97b7a1
louis.heraut <louis.heraut@inrae.fr>
2023-05-12 10:07:25 +0200

    debug

--- CARD_parser.R
+++ CARD_parser.R
@@ -30,10 +30,10 @@ parser = ArgumentParser()
 parser$add_argument('-C', '--CARD', type="character",
                     default=".",
                     help="CARD directory.")
-parser$add_argument('-d', '--directory', type="character",
+parser$add_argument('-t', '--tmp', type="character",
                     default="",
-                    help="CARD analyse writing directory.")
-parser$add_argument('-t', '--type', type="integer",
+                    help="CARD analyse temporary writing directory.")
+parser$add_argument('-n', '--n', type="integer",
                     default=1,
                     help="1 for analyse of one variable with CARD contained in the __all__/1/ directory or 2 for analyse of two variables with CARD contained in the __all__/2/ directory.")
 parser$add_argument('-l', '--layout', nargs='+', type="character",
