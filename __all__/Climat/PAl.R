bea9bd72b852c7db196cc4dd9227221aa24fc4d4
louis.heraut <louis.heraut@inrae.fr>
2023-06-12 19:43:56 +0200

    meso ready

--- __all__/Climat/PAl.R
+++ __all__/Climat/PAl.R
@@ -1,11 +1,11 @@
-CARD$P.var = "PA"
+CARD$P.var = "PAl"
 CARD$P.unit = "mm.jour^{-1}"
-CARD$P.glose = "Précipitation totale annuelle (pluie + neige)"
+CARD$P.glose = "Précipitation liquide annuelle (pluie)" 
 CARD$P.topic = "Précipitation"
 CARD$P.samplePeriod = "09-01"
 
-CARD$P1.funct = list(PA=mean)
-CARD$P1.funct_args = list("P", na.rm=TRUE)
+CARD$P1.funct = list(PAl=mean)
+CARD$P1.funct_args = list("Pl", na.rm=TRUE)
 CARD$P1.timeStep = "year"
 CARD$P1.samplePeriod = "09-01"
 CARD$P1.NApct_lim = 3
--- __all__/Climat/PAs.R
+++ __all__/Climat/PAs.R
@@ -1,11 +1,11 @@
-CARD$P.var = "PA"
+CARD$P.var = "PAs"
 CARD$P.unit = "mm.jour^{-1}"
-CARD$P.glose = "Précipitation totale annuelle (pluie + neige)"
+CARD$P.glose = "Précipitation solide annuelle (neige)"
 CARD$P.topic = "Précipitation"
 CARD$P.samplePeriod = "09-01"
 
-CARD$P1.funct = list(PA=mean)
-CARD$P1.funct_args = list("P", na.rm=TRUE)
+CARD$P1.funct = list(PAs=mean)
+CARD$P1.funct_args = list("Ps", na.rm=TRUE)
 CARD$P1.timeStep = "year"
 CARD$P1.samplePeriod = "09-01"
 CARD$P1.NApct_lim = 3
