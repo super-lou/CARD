d86e66edfa0426679c9ea77e479c08def6a44991
louis.heraut <louis.heraut@inrae.fr>
2023-08-22 14:02:02 +0200

    fdzeojfez

--- __all__/Sensibilite_a_la_variabilite_climatique/Rc.R
+++ __all__/Sensibilite_a_la_variabilite_climatique/Rc.R
@@ -3,10 +3,10 @@ CARD$P.unit = "m^{3}.s^{-1}.mm^{-1}"
 CARD$P.is_date = FALSE
 CARD$P.normalize = TRUE
 CARD$P.reverse_palette = FALSE
-CARD$P.glose = "Coefficient de ruissellement, rapport entre la somme des débits et la somme des précipitations liquides"
+CARD$P.glose = "Coefficient de ruissellement, rapport entre la somme des débits et la somme des précipitations"
 CARD$P.topic = c("Précipitations/Débit", "Sensibilité à la variabilité climatique", "Signature hydrologique")
 
 CARD$P1.funct = list(Rc=compute_Rc)
-CARD$P1.funct_args = list("Q", "Pl")
+CARD$P1.funct_args = list("Q", "P")
 CARD$P1.timeStep = "none"
 CARD$P1.NAyear_lim = 10
