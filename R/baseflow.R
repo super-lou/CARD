a2df2c6aed33c9f89e5ebc633bb568ec3f1f6259
Louis Héraut <louis.heraut@inrae.fr>
2025-04-22 11:52:11 +0200

    debug

--- R/baseflow.R
+++ R/baseflow.R
@@ -169,7 +169,6 @@ dBFS = function (Q, d=5, w=0.9, a=0.925, passes=3, method='Wal') {
 
 
 verif_LH = function (na.omit=FALSE) {
-    library(plotly)
     Q = ASHE::create_data_HYDRO(
                   "/home/louis/Documents/bouleau/INRAE/data/hydrologie",
                   "AEAG_selection", "O0362510_HYDRO_QJM.txt", "Qm3s")$Q
@@ -187,30 +186,30 @@ verif_LH = function (na.omit=FALSE) {
     BFgrwat = grwat::gr_baseflow(Q, a=a, passes=passes, padding=0)
     BFadc = adc::bf_sep_lh(Q, a=a, n=passes, reflect=0) 
 
-    plot = plot_ly()
-    plot =  add_lines(plot,
-                      x=X,
-                      y=Q,
-                      name="Q",
-                      line=list(color='blue'))
-    plot = add_lines(plot,
-                     x=X,
-                     y=BFgrwat,
-                     name="BF_LH_grwat",
-                     line=list(color='black'))
-    plot = add_lines(plot,
-                     x=X,
-                     y=BFadc,
-                     name="BF_LH_adc",
-                     line=list(color='green'))
-    plot = add_lines(plot,
-                     x=X,
-                     y=BFme,
-                     name="BR_LH_louis",
-                     line=list(color='red'))
-    plot = layout(plot,
-                  xaxis=list(title="step"),
-                  yaxis=list(title="m3s-1"))
+    plot = plotly::plot_ly()
+    plot =  plotly::add_lines(plot,
+                              x=X,
+                              y=Q,
+                              name="Q",
+                              line=list(color='blue'))
+    plot = plotly::add_lines(plot,
+                             x=X,
+                             y=BFgrwat,
+                             name="BF_LH_grwat",
+                             line=list(color='black'))
+    plot = plotly::add_lines(plot,
+                             x=X,
+                             y=BFadc,
+                             name="BF_LH_adc",
+                             line=list(color='green'))
+    plot = plotly::add_lines(plot,
+                             x=X,
+                             y=BFme,
+                             name="BR_LH_louis",
+                             line=list(color='red'))
+    plot = plotly::layout(plot,
+                          xaxis=list(title="step"),
+                          yaxis=list(title="m3s-1"))
 
     filename = paste0("BF_LH", ".html")
     htmlwidgets::saveWidget(plot, file=filename)
