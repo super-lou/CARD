30353e9fe1c7a7155977062e4de0248a18c46789
Louis Héraut <louis.heraut@inrae.fr>
2025-04-30 18:55:45 +0200

    add a CARD_name column to csv file that list all CRAD

--- dev/get_metaEX_all.R
+++ dev/get_metaEX_all.R
@@ -6,5 +6,8 @@ res = CARD_extraction(data=NULL,
                       CARD_name=NULL,
                       extract_only_metadata=TRUE)
 
+res$metaEX$CARD_name = res$metaEX$variable_en
+res$metaEX = dplyr::relocate(res$metaEX, CARD_name, .before=variable_en)
+
 ASHE::write_tibble(res$metaEX, file.path("inst", "extdata",
                                          "metaEX_all.csv"))
