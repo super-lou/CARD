d614788fa6055734de65d7a20d72ad982e9a00dd
Louis Héraut <louis.heraut@inrae.fr>
2025-05-05 11:39:20 +0200

    add yaml example and move a dev function from R to dev

--- R/baseflow.R
+++ R/baseflow.R
@@ -168,53 +168,6 @@ dBFS = function (Q, d=5, w=0.9, a=0.925, passes=3, method='Wal') {
 }
 
 
-verif_LH = function (na.omit=FALSE) {
-    Q = ASHE::create_data_HYDRO(
-                  "/home/louis/Documents/bouleau/INRAE/data/hydrologie",
-                  "AEAG_selection", "O0362510_HYDRO_QJM.txt", "Qm3s")$Q
-    Q[c(32921, 32922, 32926)] = NA
-
-    if (na.omit) {
-        Q = Q[!is.na(Q)]
-    }
-    
-    a = 0.925
-    passes = 3
-
-    X = 1:length(Q)
-    BFme = BFS(Q, a=a, passes=passes, method='LH')
-    BFgrwat = grwat::gr_baseflow(Q, a=a, passes=passes, padding=0)
-    BFadc = adc::bf_sep_lh(Q, a=a, n=passes, reflect=0) 
-
-    plot = plotly::plot_ly()
-    plot =  plotly::add_lines(plot,
-                              x=X,
-                              y=Q,
-                              name="Q",
-                              line=list(color='blue'))
-    plot = plotly::add_lines(plot,
-                             x=X,
-                             y=BFgrwat,
-                             name="BF_LH_grwat",
-                             line=list(color='black'))
-    plot = plotly::add_lines(plot,
-                             x=X,
-                             y=BFadc,
-                             name="BF_LH_adc",
-                             line=list(color='green'))
-    plot = plotly::add_lines(plot,
-                             x=X,
-                             y=BFme,
-                             name="BR_LH_louis",
-                             line=list(color='red'))
-    plot = plotly::layout(plot,
-                          xaxis=list(title="step"),
-                          yaxis=list(title="m3s-1"))
-
-    filename = paste0("BF_LH", ".html")
-    htmlwidgets::saveWidget(plot, file=filename)
-}
-
 
 ## 2. BASEFLOW INDEX _________________________________________________
 #' @title baseflow_index
--- dev/test.R
+++ dev/test.R
@@ -1,35 +1,35 @@
 
 devtools::load_all()
 
-CARD_name = list("Température"=c("TMA_month", "TSA_season", "TA"),
-                 "Hautes Eaux"=c("QJXA", "tQJXA", "VCX3",
-                                 "tVCX3", "VCX10", "tVCX10",
-                                 "Q01A", "Q05A", "Q10A",
-                                 "fQ01A", "fQ05A", "fQ10A",
-                                 "dtFlood"),
-                 "Moyennes Eaux"=c("QA", "Q75A", "Q50A",
-                                   "Q25A", "QSA_season", "QMA_month"),
-                 "Basses Eaux"=c("VCN10", "tVCN10", "QNA",
-                                 "QMNA", "VCN3", "VCN30",
-                                 "Q90A", "Q95A", "Q99A",
-                                 "startLF", "centerLF", "endLF",
-                                 "dtLF", "vLF"),
-                 "Débit de Base"=c("startBF", "centerBF", "endBF",
-                                   "dtBF", "vBF"),
-                 "Précipitations"=c("RCXA1", "RCXA5", "dtRA20mm",
-                                    "dtRSA20mm_season",
-                                    "dtRMA20mm_month",
-                                    "dtRA50mm", "dtRSA50mm_season",
-                                    "dtRMA50mm_month", "RA",
-                                    "RSA_season", "RMA_month", "RAl",
-                                    "RSAl_season", "RMAl_month",
-                                    "RAl_ratio", "RAs", "RSAs_season",
-                                    "RMAs_month", "RAs_ratio",
-                                    "dtRA01mm", "dtRSA01mm_season",
-                                    "dtRMA01mm_month", "dtCWDA",
-                                    "dtCWDSA_season", "dtCWDMA_month",
-                                    "dtCDDA", "dtCDDSA_season",
-                                    "dtCDDMA_month"))
+# CARD_name = list("Température"=c("TMA_month", "TSA_season", "TA"),
+#                  "Hautes Eaux"=c("QJXA", "tQJXA", "VCX3",
+#                                  "tVCX3", "VCX10", "tVCX10",
+#                                  "Q01A", "Q05A", "Q10A",
+#                                  "fQ01A", "fQ05A", "fQ10A",
+#                                  "dtFlood"),
+#                  "Moyennes Eaux"=c("QA", "Q75A", "Q50A",
+#                                    "Q25A", "QSA_season", "QMA_month"),
+#                  "Basses Eaux"=c("VCN10", "tVCN10", "QNA",
+#                                  "QMNA", "VCN3", "VCN30",
+#                                  "Q90A", "Q95A", "Q99A",
+#                                  "startLF", "centerLF", "endLF",
+#                                  "dtLF", "vLF"),
+#                  "Débit de Base"=c("startBF", "centerBF", "endBF",
+#                                    "dtBF", "vBF"),
+#                  "Précipitations"=c("RCXA1", "RCXA5", "dtRA20mm",
+#                                     "dtRSA20mm_season",
+#                                     "dtRMA20mm_month",
+#                                     "dtRA50mm", "dtRSA50mm_season",
+#                                     "dtRMA50mm_month", "RA",
+#                                     "RSA_season", "RMA_month", "RAl",
+#                                     "RSAl_season", "RMAl_month",
+#                                     "RAl_ratio", "RAs", "RSAs_season",
+#                                     "RMAs_month", "RAs_ratio",
+#                                     "dtRA01mm", "dtRSA01mm_season",
+#                                     "dtRMA01mm_month", "dtCWDA",
+#                                     "dtCWDSA_season", "dtCWDMA_month",
+#                                     "dtCDDA", "dtCDDSA_season",
+#                                     "dtCDDMA_month"))
 
 
 # CARD_name=list(A=c("QA", "QMNA"),
@@ -40,7 +40,7 @@ CARD_name = list("Température"=c("TMA_month", "TSA_season", "TA"),
 
 # CARD_name=c("QA", "QMNA")
 
-CARD_management(CARD_name=CARD_name, overwrite=TRUE)
+# CARD_management(CARD_name=CARD_name, overwrite=TRUE)
 
 
 
@@ -57,3 +57,6 @@ CARD_management(CARD_name=CARD_name, overwrite=TRUE)
 
 # res = CARD_extraction(data, CARD_name=c("dtRec", "med{dtRec}"))
 # res
+
+
+Process = sourceProcess("../inst/__all__/Flow/Low_Flows/serie/startLF.R")
